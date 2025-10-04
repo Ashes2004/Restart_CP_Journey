22f5946426045f84bdf3115ce8beeacbe1fae0de
Ashes Das <127014640+Ashes2004@users.noreply.github.com>
2025-10-04 19:20:27 +0530

    Create 433A.cpp

---
+++ RatingWise/900/433A.cpp
@@ -0,0 +1,24 @@
+// A. Anton and Letters
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	string str;
+	getline(cin , str);
+	
+	set<char>st;
+
+	
+	for(int i = 0 ; i<str.size() ; i++)
+	{
+	    if(str[i] >= 'a' && str[i] <= 'z')
+	    {
+	        st.insert(str[i]);
+	       
+	    }
+	}
+	
+	
+	cout<<st.size()<<endl;
+
+}
